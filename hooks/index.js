b4df03b83665af2e42e547e941206298d9527262
Maksym Pinchuk <maksympc@gmail.com>
2020-04-12 15:04:32 +0300

    implement useEffect hook

--- hooks/index.js
+++ hooks/index.js
@@ -1,7 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import randomcolor from 'randomcolor'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('black');
+
+  useEffect(() => {
+    setColor(randomcolor())
+  }, [count])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setCount(prev => prev + 1), 1000);
+    return () => clearInterval(intervalId);
+  }, [])
 
   const increment = () => setCount(prevCount => prevCount + 1);
   const decrement = () => setCount(prevCount => prevCount - 1);
@@ -10,7 +21,7 @@ function App() {
 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1 style={{color: color}}>{count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={double}>Double</button>
